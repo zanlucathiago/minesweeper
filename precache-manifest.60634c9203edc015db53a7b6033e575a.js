e7dbb13abc962799608447d0cb15d1dca9f914e6
Thiago Monteiro Zanluca <zanlucathiago@gmail.com>
2020-03-10 01:17:23 -0300

    Updates

---
+++ precache-manifest.60634c9203edc015db53a7b6033e575a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3011878a46fcdeb6293cffd0aab37139",
+    "url": "./index.html"
+  },
+  {
+    "revision": "413a8eeb9e0a3b7bba44",
+    "url": "./static/css/main.7e59c01b.chunk.css"
+  },
+  {
+    "revision": "4a0a947f0f78e7f21dab",
+    "url": "./static/js/2.70abbd65.chunk.js"
+  },
+  {
+    "revision": "0b1e8647e82c18f1bbe8b4a59fb3fbb8",
+    "url": "./static/js/2.70abbd65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "413a8eeb9e0a3b7bba44",
+    "url": "./static/js/main.738ac613.chunk.js"
+  },
+  {
+    "revision": "9827486d1ee576d5ae9e",
+    "url": "./static/js/runtime-main.cc3b2f18.js"
+  }
+]);
