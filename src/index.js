35e1c877210105738b28829db5e24ccb0a2f2c00
Thiago Monteiro Zanluca <zanlucathiago@gmail.com>
2020-05-30 14:10:43 -0300

    Limpeza

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Socket from './services/Socket';
 
-// eslint-disable-next-line
 ReactDOM.render(<Socket />, document.getElementById('root'));
